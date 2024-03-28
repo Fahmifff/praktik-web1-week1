28d3cbdaa4cba0dbce00c588694dbe619b530f4c
Fahmifff <164866034+Fahmifff@users.noreply.github.com>
2024-03-28 21:57:18 +0700

    Add files via upload

---
+++ praktik-pweb1-week1/Latihan/latihan-uts-2/script.js
@@ -0,0 +1,8 @@
+function ubah(){
+    document.getElementById('para').innerHTML="Paragraf akhir";
+    alert('Paragraf berhasil diubah');
+}
+function balik(){
+    document.getElementById('para').innerHTML="Paragraf awal";
+    alert('Paragraf berhasil dikembalikan');
+}
---
+++ praktik-pweb1-week1/Latihan/latihan-uts/script.js
@@ -0,0 +1,7 @@
+function mau(){
+    alert('hasilnya adalah 2');
+}
+
+function gas(){
+    document.getElementById('has').innerHTML="hasilnya adalah 2";
+}
